9bcf65b51f8d3b34429aa2a17c0a596817075acc
henry <henry@iMac-home.local>
2022-03-15 23:35:40 +0900

    ....update

--- client/src/components/Presentation/Intro.js
+++ client/src/components/Presentation/Intro.js
@@ -1,6 +1,6 @@
 import React from "react";
-import styled from "styled-components";
 import { motion } from "framer-motion";
+import styled from "styled-components";
 
 const Wrapper = styled.div`
   height: 100vh;
